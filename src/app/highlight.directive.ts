94ff08a6b64ae4483e177d35a93ab26a9e3ae5f3
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-06-23 10:34:26 -0500

    bind to a second property

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,13 +6,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class HighlightDirective {
 
   @Input('appHighlight') highlightColor: string
+  @Input() public defaultColor: string;
 
   constructor(private el: ElementRef) {
 
   }
 
   @HostListener('mouseenter') onMouseEnter(){
-    this.highlight(this.highlightColor || 'red');
+    this.highlight(this.highlightColor || this.defaultColor || 'red');
   }
 
   @HostListener('mouseleave') onMouseLeave(){
